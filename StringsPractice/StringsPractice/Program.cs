cab1cef95becd7e240519a9b9f56f4c40f63de7d
Artem Shishov <shishov1610@gmail.com>
2025-11-22 16:16:09 +0300

    Added new project StringPractice

---
+++ StringsPractice/StringsPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

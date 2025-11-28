384371e391f83d45dd698645ef7eaeb5bba29763
Artem Shishov <shishov1610@gmail.com>
2025-11-29 00:18:12 +0300

    Added new project OOPIntro. Added first task.

---
+++ OOPIntro/OOPIntro/PhoneClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPIntro
+{
+    public class PhoneClass
+    {
+        public PhoneClass(string number, string model, string weigth) : this(number, model)
+        {
+            this._weight = weigth;
+        }
+
+        public PhoneClass(string number, string model)
+        {
+            this._number = number;
+            this._model = model;
+        }
+
+        public PhoneClass()
+        {
+        }
+        public string Number 
+        {
+            get => _number;
+            set => _number = string.IsNullOrEmpty(value) ? " " : value;
+        }
+
+        public string Model
+        {
+            get => _model;
+            set => _model = string.IsNullOrEmpty(value) ? " " : value;
+        }
+
+        public string Weigth
+        {
+            get => _weight;
+            set => _weight = string.IsNullOrEmpty(value) ? " " : value;
+        }
+
+        public void RecieveCall(string callerName)
+        {
+            Console.WriteLine($"Звонит {callerName}");
+        }
+
+        public void RecieveCall(string callerName, string callerNumber)
+        {
+            Console.WriteLine($"Звонит {callerName} с номера {callerNumber}");
+        }
+
+        public string GetNumber()
+        {
+            return this._number;
+        }
+
+        public void SendMessage(params string[] numbers)
+        {
+            Console.Write("\tПолучатели сообщений: ");
+            Array.ForEach(numbers, number => Console.Write(number + "; "));
+        }
+
+        #region private
+
+        private string _number;
+        private string _model;
+        private string _weight;
+
+        #endregion
+    }
+}
---
+++ OOPIntro/OOPIntro/Program.cs
@@ -0,0 +1,114 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace OOPIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите номер задания:\n" +
+                    "\t1. Задание 1.\n" +
+                    "\t2. Задание 2.\n" +
+                    "Нажмите 'q' чтобы завершить работу.");
+
+                var key = Console.ReadKey(true);
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        Console.Clear();
+                        Task1();
+                        WaitFun();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        Task2();
+                        WaitFun();
+                        break;
+                    case 'q':
+                    case 'Q':
+                        Console.WriteLine("Завершение работы.");
+                        return;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+
+        static void WaitFun()
+        {
+            Console.WriteLine("\n\nНажмите любую клавишу чтобы продолжить.");
+
+            var key = Console.ReadKey(true);
+            switch (key.KeyChar)
+            {
+                default:
+                    Console.Clear();
+                    return;
+            }
+        }
+
+        static void Task1()
+        {
+            PhoneClass phone1 = new PhoneClass();
+            phone1.Number = "+375 (29) 111-00-11";
+            phone1.Model = "iPhone 17";
+            phone1.Weigth = "177г";
+
+            PhoneClass phone2 = new PhoneClass();
+            phone2.Number = "+375 (29) 222-00-22";
+            phone2.Model = "Samsung S25";
+            phone2.Weigth = "162г";
+
+            PhoneClass phone3 = new PhoneClass();
+            phone3.Number = "+375 (29) 333-00-33";
+            phone3.Model = "Xiaomi 15T";
+            phone3.Weigth = "194г";
+
+            PrintPhoneParams(phone1, nameof(phone1));
+            PrintPhoneParams(phone2, nameof(phone2));
+            PrintPhoneParams(phone3, nameof(phone3));
+
+            PrintByBaseMethods(phone1, nameof(phone1));
+            PrintByBaseMethods(phone2, nameof(phone2));
+            PrintByBaseMethods(phone3, nameof(phone3));
+
+            PhoneClass phone4 = new PhoneClass("+375 (29) 444-00-44", "POCO F7", "215.7г");
+            PrintPhoneParams(phone4, nameof(phone4));
+
+            PrintByOverloadedMethod(phone1, nameof(phone1), phone2);
+            PrintByOverloadedMethod(phone2, nameof(phone2), phone3);
+            PrintByOverloadedMethod(phone3, nameof(phone3), phone4);
+            PrintByOverloadedMethod(phone4, nameof(phone4), phone1);
+
+            Console.WriteLine($"\n{nameof(phone1)}:");
+            phone1.SendMessage(phone2.GetNumber(), phone3.GetNumber(), phone4.GetNumber());
+        }
+        static void PrintPhoneParams(PhoneClass phone, string objectName)
+        {
+            Console.WriteLine($"{objectName}:\n" +
+                $"\tМодель: {phone.Model}\n" +
+                $"\tВес: {phone.Weigth}\n" +
+                $"\tНомер телефона: {phone.Number}");
+        }
+
+        static void PrintByBaseMethods(PhoneClass phone, string objectName)
+        {
+            Console.Write($"{objectName}, номер телефона {phone.GetNumber()}: ");
+            phone.RecieveCall("Антон");
+        }
+
+        static void PrintByOverloadedMethod(PhoneClass userPhone, string objectName, PhoneClass callerPhone)
+        {
+            Console.Write($"{objectName}: ");
+            userPhone.RecieveCall("Антон", callerPhone.GetNumber());
+        }
+
+        static void Task2()
+        {
+
+        }
+    }
+}

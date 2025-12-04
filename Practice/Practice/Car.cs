1765e40d523a4b252b73d7fbf15bd99b23edd44b
Artem Shishov <shishov1610@gmail.com>
2025-12-04 21:58:07 +0300

    Added project

---
+++ Practice/Practice/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal abstract class Car: IVehicle
+    {
+        public Car(int iniFuel, int consumption)
+        {
+            Fuel = iniFuel;
+            Consumption = int.IsNegative(consumption) ? 0 : consumption;
+        }
+
+        public virtual void Drive(int distance)
+        {
+            Fuel = distance * Consumption;
+        }
+
+        public virtual bool Refuel(int gasoline)
+        {
+            if (gasoline > 0)
+            {
+                Fuel += gasoline;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Fuel { get; set; }
+        public int Consumption { get; set; }
+    }
+}
---
+++ Practice/Practice/Truck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Truck : Car
+    {
+        public Truck(int iniFuel, int consumption) : base(iniFuel, consumption)
+        {
+        }
+
+        public override void Drive(int distance)
+        {
+            int fuelLost = distance * Consumption;
+            if (Fuel >= fuelLost)
+            {
+                Console.WriteLine("Truck движется.");
+                Fuel -= fuelLost;
+            }
+            else
+            {
+                Console.WriteLine($"Truck не движется. Не хватит топлива для прохождения {distance} растояния.");
+            }
+        }
+
+        public override bool Refuel(int gasoline)
+        {
+            if (gasoline > 0)
+            {
+                Console.WriteLine($"Truck дозаправился на {gasoline}.");
+                Fuel += gasoline;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Truck не может заправится на {gasoline}.");
+                return false;
+            }
+        }
+    }
+}

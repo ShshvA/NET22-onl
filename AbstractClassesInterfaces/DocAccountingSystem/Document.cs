a69990fbd5afbd7d74e15604cda4ecefce91580d
Artem Shishov <shishov1610@gmail.com>
2025-12-07 16:57:18 +0300

    Added additional task

---
+++ AbstractClassesInterfaces/DocAccountingSystem/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocAccountingSystem
+{
+    internal abstract class Document
+    {
+        public virtual void GetDocInfo()
+        {
+            Console.WriteLine($"Информация о документе:\n" +
+                $"\t Тип: {Type} \n" +
+                $"\t Номер: {DocNumber} \n" +
+                $"\t Дата: {Date.ToLocalTime().ToString("dd.MM.yyyy HH:mm:ss")}");
+        }
+
+        public string DocNumber { get; protected set; } = Guid.NewGuid().ToString();
+
+        public DateTime Date { get; protected set; } = DateTime.UtcNow;
+
+        public string Type { get; protected set; }
+    }
+}
---
+++ AbstractClassesInterfaces/DocAccountingSystem/FinancialInvoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocAccountingSystem
+{
+    internal class FinancialInvoice : Document
+    {
+
+        public FinancialInvoice()
+        {
+            base.Type = "EmployeeContract";
+        }
+
+        public FinancialInvoice(double monthAmount, int code) : this()
+        {
+            TotalMonthAmount = double.IsNegative(monthAmount) ? 0 : monthAmount;
+            DepartmentCode = int.IsNegative(code) ? 0 : code;
+        }
+
+        public override void GetDocInfo()
+        {
+            base.GetDocInfo();
+            Console.WriteLine($"\t Итоговая сумма за месяц: {TotalMonthAmount} \n" +
+                $"\t Код департамента: {(DepartmentCode == 0 ? "Отсутствует" : DepartmentCode)} \n");
+        }
+
+        public double TotalMonthAmount { get; set; } = 0;
+
+        public int DepartmentCode { get; set; } = 0;
+    }
+}
